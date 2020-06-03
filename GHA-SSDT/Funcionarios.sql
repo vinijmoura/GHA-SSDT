bbdbe0fc426aaf0cf6a8e277e46c3d6082b7fa83
Vinicius Moura <v-vimou@microsoft.com>
2020-06-03 15:58:15 -0300

    Inclusao de novo campo

--- GHA-SSDT/Funcionarios.sql
+++ GHA-SSDT/Funcionarios.sql
@@ -2,5 +2,6 @@
 (
 	[Id] INT IDENTITY(1,1) NOT NULL PRIMARY KEY, 
     [Nome] VARCHAR(50) NOT NULL, 
-    [NumeroCracha] NCHAR(10) NOT NULL
+    [NumeroCracha] NCHAR(10) NOT NULL, 
+    [Endereco] VARCHAR(50) NULL
 )
