5ccb3b1d36929207db757092e2b134d6677cef26
Vinicius Moura <v-vimou@microsoft.com>
2020-08-08 16:41:17 -0300

    nova coluna Telefone

--- GHA-SSDT/Funcionarios.sql
+++ GHA-SSDT/Funcionarios.sql
@@ -3,5 +3,6 @@
 	[Id] INT IDENTITY(1,1) NOT NULL PRIMARY KEY, 
     [Nome] VARCHAR(50) NOT NULL, 
     [NumeroCracha] NCHAR(10) NOT NULL, 
-    [Endereco] VARCHAR(50) NULL
+    [Endereco] VARCHAR(50) NULL, 
+    [Telefone] VARCHAR(50) NULL
 )
