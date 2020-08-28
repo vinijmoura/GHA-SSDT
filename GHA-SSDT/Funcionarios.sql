83220dc29c966bf86936175991400d7bf2862cec
Vinicius Moura <v-vimou@microsoft.com>
2020-08-28 19:27:52 -0300

    atualização de projeto!

--- GHA-SSDT/Funcionarios.sql
+++ GHA-SSDT/Funcionarios.sql
@@ -1,11 +1,14 @@
-﻿CREATE TABLE [dbo].[Funcionarios]
-(
-	[Id] INT IDENTITY(1,1) NOT NULL PRIMARY KEY, 
-    [Nome] VARCHAR(50) NOT NULL, 
-    [NumeroCracha] NCHAR(10) NOT NULL, 
-    [Endereco] VARCHAR(50) NULL, 
-    [Telefone] VARCHAR(50) NULL, 
-    [CPF] VARCHAR(11) NULL, 
-    [RG] VARCHAR(10) NULL, 
-    [Celular] VARCHAR(12) NULL
-)
+﻿CREATE TABLE [dbo].[Funcionarios] (
+    [Id]             INT          IDENTITY (1, 1) NOT NULL,
+    [Nome]           VARCHAR (50) NOT NULL,
+    [NumeroCracha]   NCHAR (10)   NOT NULL,
+    [Endereco]       VARCHAR (50) NULL,
+    [Telefone]       VARCHAR (50) NULL,
+    [CPF]            VARCHAR (11) NULL,
+    [RG]             VARCHAR (10) NULL,
+    [Celular]        VARCHAR (12) NULL,
+    [numeroendereco] VARCHAR (8)  NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
+
