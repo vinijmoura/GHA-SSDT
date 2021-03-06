a93fead058622e9dc0cd533553caa77641a48eff
Vinicius Moura <v-vimou@microsoft.com>
2021-03-06 10:08:45 -0300

    inclusao do campo TituloEleitor

--- GHA-SSDT/Funcionarios.sql
+++ GHA-SSDT/Funcionarios.sql
@@ -8,7 +8,8 @@
     [RG]             VARCHAR (10) NULL,
     [Celular]        VARCHAR (12) NULL,
     [numeroendereco] VARCHAR (8)  NULL,
-    [ComplmentoEndereco] VARCHAR (30) NULL
+    [ComplmentoEndereco] VARCHAR (30) NULL,
+    [TituloEleitor] VARCHAR(20) NULL
     PRIMARY KEY CLUSTERED ([Id] ASC)
 );
 
